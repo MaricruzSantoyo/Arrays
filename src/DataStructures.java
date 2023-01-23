a8dd74916e6355f9c9df7fa2ebeeea0c4d4d666a
Maricruz Santoyo <msantoy3@asu.edu>
2023-01-22 23:23:08 -0700

    finalized

--- src/DataStructures.java
+++ src/DataStructures.java
@@ -1,17 +1,5 @@
 
 public class DataStructures {
-	
-	//1st part of part 3:
-
-	public static int[] toPower(int size, int power) {
-		int[] returnArray = new int[size];
-		for (int i = 0; i < returnArray.length; ++i) {
-			returnArray[i] = (int) Math.pow(i, power);
-		}
-		return returnArray;
-	}
-	
-	//Part 1:
 
 	public static void main(String[] args) {
 		int[] myArray = { 1, 2, 3, 4, 5 };
@@ -22,42 +10,7 @@ public class DataStructures {
 		}
 
 		System.out.println(sum);
-		
-		// Part 2:
-		
-		System.out.println("----------");
 
-		double[] exampleArray = { 1, 5, 6, 5, 4, 1 };
-
-		double maximum = exampleArray[0];
-
-		int index = 0;
-
-		for (int i = 1; i < exampleArray.length; i++) {
-
-			if (exampleArray[i] > maximum) {
-
-				maximum = exampleArray[i];
-
-				index = i;
-
-			}
-
-		}
-
-		System.out.println(index);
-
-		// the system outputs to 2.
-		
-		
-		//2nd half of part 3:
-
-		System.out.println("----------");
-
-		int[] result = toPower(4, 2);
-		for (int i = 0; i < result.length; ++i) {
-			System.out.println(result[i]);
-		}
 	}
 
 }
